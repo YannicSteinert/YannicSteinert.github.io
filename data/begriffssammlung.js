50c161c2156393046a28101a209c5520dd1e5191
YannicSteinert <74675755+YannicSteinert@users.noreply.github.com>
2020-11-20 10:42:22 +0100

    Add files via upload

---
+++ data/begriffssammlung.js
@@ -0,0 +1,4 @@
+var begriffssammlung_json='[' +
+	'{"Anzeigename":"GRUB"			,"Kathegorie":"Sofware"			,"Text1":"",		"Bild1":"",		"Text2":"",		"Bild2":"",		"Text3":""},'+
+	'{"Anzeigename":"mbr"			,"Kathegorie":"Sofware"			,"Text1":"",		"Bild1":"",		"Text2":"",		"Bild2":"",		"Text3":""},'+
+	'{"Anzeigename":"SHA"			,"Kathegorie":"Verschlüsslung"	,"Text1":"",		"Bild1":"",		"Text2":"",		"Bild2":"",		"Text3":""}]';
---
+++ data/datein.js
@@ -0,0 +1,4 @@
+var datein_json='[' +
+	'{"Anzeigename":"/etc/passwd"			,"Kathegorie":"Passwortverwaltung"			,"Text1":"",		"Bild1":"",		"Text2":"",		"Bild2":"",		"Text3":""},'+
+	'{"Anzeigename":"/etc/shadow"			,"Kathegorie":"Passwortverwaltung"			,"Text1":"",		"Bild1":"",		"Text2":"",		"Bild2":"",		"Text3":""},'+
+	'{"Anzeigename":"/etc/goup"				,"Kathegorie":"Nutzerverwaltung"			,"Text1":"",		"Bild1":"",		"Text2":"",		"Bild2":"",		"Text3":""}]';
